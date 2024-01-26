495ee4d384dc47946c13fb8b4dc63d284dead93e
Kapukankanamage Don Supun Wathsana Weerasinghe <spn.weer@gmail.com>
2024-01-26 10:58:29 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+<!-- this is printing hello world -->    
+<?php
+// this is printing hello world
+/*multiple
+line comments
+
+*/
+echo "<h1>Hello World</h1>";
+?>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+<?php
+echo "Hello World";
+?>
