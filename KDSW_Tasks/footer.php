bb3560db0f0bb2770af130ddda01d76f72eebd96
Kapukankanamage Don Supun Wathsana Weerasinghe <spn.weer@gmail.com>
2024-01-29 11:52:28 +0200

    ex2

---
+++ KDSW_Tasks/footer.php
@@ -0,0 +1,42 @@
+</div>
+<footer>
+        
+        <div class="cards2">
+          <div class="card">
+            <img src="images/frame1/logo.gif" alt="image"  class="rounded mx-auto d-block img-fluid" width="75" height="50">  
+            <p style="text-align: center;"><br>Tourism Hotline<br>1912</p>
+          </div>
+      
+          <div class="card">
+          <p style="text-align: center;">Quick Links</p>
+          <a href="">Subscribe</a>
+          <a href="">Contact Us          </a>
+          <a href="">Important Notices          </a>
+          <a href="">Terms of Use</a>
+          <a href="">Complaints</a>
+          <p style="text-align: center;"><br><br>&copy; 2024 All Rights Reserved by Team 12</p>
+          
+
+          </div>
+      
+          <div class="card">
+            <p style="text-align: center;">Follow Us On</p>   
+           <a href=""> <img src="images/frame3/facebook.png" alt="image"  width="50" height="50" style="padding: 5px;"></a>
+           <a href=""> <img src="images/frame3/instagram.png" alt="image" width="50" height="50" style="padding: 5px;"></a>
+           <a href=""> <img src="images/frame3/twitter.png" alt="image" width="50" height="50" style="padding: 5px;"></a>
+           <a href=""> <img src="images/frame3/whatsapp.png" alt="image" width="50" height="50" style="padding: 5px;"></a>
+
+              
+          </div>
+       
+      
+      
+          
+        </div>
+        
+
+      </footer>
+  </div>   
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ KDSW_Tasks/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sri Lanka</title>
+    <link rel="icon" href="images/frame1/sri-lanka.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Allan&family=Allerta&family=Allura&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style/style.css">
+
+</head>
+<body>
+  <div class="container" >
+  
+    <header >
+        <nav class="navbar navbar-expand-lg fixed-top  bg-light">
+            <div class="container-fluid">
+              
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="#page1"><img src="images/frame1/logo.gif" alt="logo" width="90" height="40"></a>
+                      </li>
+                  <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#2">Plan your trip</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#3">Where to go </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#4"> Events </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#5">Adventure </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#6">Accormadation </a>
+                  </li>
+                  
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#7" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Foods
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#7">Main Dishes
+                      </a></li>
+                      <li><a class="dropdown-item" href="#8">Spices</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#9">Sea Food
+                      </a></li>
+                    </ul>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </nav>
+        
+    </header>
+    <div id="page1" style="padding-top: 60px;" >
