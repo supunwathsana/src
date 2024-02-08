8dd9c3b9ecc8e2d21576b8f256f58c582d92d2c2
Kapukankanamage Don Supun Wathsana Weerasinghe <spn.weer@gmail.com>
2024-02-08 09:52:40 +0200

    changesinaboutus

--- KDSW_Tasks/javascript/aboutus.php
+++ KDSW_Tasks/javascript/aboutus.php
@@ -6,6 +6,9 @@
     document.write ("Hello World")
     </script>
 
+<noscript>
+    If javascript is not enabled this will be displayed so users know they need to enable js.
+</noscript>
 
 
 <?php include '../footer.php'; ?> 
