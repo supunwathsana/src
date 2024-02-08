c1d40707f01064b163f62bce666b388cdc2877c6
Kapukankanamage Don Supun Wathsana Weerasinghe <spn.weer@gmail.com>
2024-02-08 10:32:25 +0200

    aboutus

--- KDSW_Tasks/javascript/aboutus.php
+++ KDSW_Tasks/javascript/aboutus.php
@@ -3,12 +3,23 @@
 <h2>About Us</h2>
 
 <script>
+    // this is a comment
     document.write ("Hello World")
+    // triggering an alert box
+    //window.alert("This is an alert box")
     </script>
 
 <noscript>
     If javascript is not enabled this will be displayed so users know they need to enable js.
 </noscript>
+
+<script>
+    //document.write but after loading the page
+    function hello(){
+            document.write("This is after the page has loaded")
+    }
+    </script>
+    <button onclick="document.write('This is something')">Click Me</button>
 
 
 <?php include '../footer.php'; ?> 
