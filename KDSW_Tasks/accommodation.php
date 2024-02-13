325f2a34f09723aebb5833fdfb1e981644458044
Kapukankanamage Don Supun Wathsana Weerasinghe <spn.weer@gmail.com>
2024-02-13 10:07:48 +0200

    travelupdate

---
+++ KDSW_Tasks/accommodation.php
@@ -0,0 +1,8 @@
+<?php
+$pagetitle="Accommodation";
+include 'header.php';?>
+
+<h3>JAFFNA HOTELS</h3>
+
+<img src="./images/24.png" alt="web development environment">
+     <?php include 'footer.php'; ?>
